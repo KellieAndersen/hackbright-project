67c4c69c0660a1bc2504ee2169ee66c548d4c0d0
vagrant <vagrant@vagrant.vm>
2021-04-06 20:22:52 +0000

    complete model.py before testing everything

--- model.py
+++ model.py
@@ -22,7 +22,8 @@ class Recipes(db.Model):
 
     def __repr__(self):
         """Show info about recipe"""
-        return f'< The recipe is {self.recipe_name}, from the kitchen of {self.originator}>'
+        return f'< The recipe is {self.recipe_name}, 
+        from the kitchen of {self.originator}, recipe_id {self.recipe_id}>'
 
 
 class Ratings(db.Model):
@@ -30,12 +31,17 @@ class Ratings(db.Model):
 
     __tablename__ = 'rating'
 
-    rating = db.Coloumn(db.Integer,
-                        primary_key = True,)
+    rating_id = db.Coloumn(db.Integer,
+                        primary_key = True,
+                        autoincrement = True, )
+    rating = db.Column(db.Integer,)
     recipe_id = db.Column(db.Integer,
                         db.ForeignKey('recipes.recipe_id'), )
 
+    def __repr__(self):
+        return f'<recipe_id {self.recipe_id} has a rating of {rating}>'
 
+    
 class Notes(db.Model):
     """Notes about a recipe"""
 
@@ -48,6 +54,8 @@ class Notes(db.Model):
                         db.ForeignKey('recipes.recipe_id'), )
     note = db.Column(db.Text, )
 
+    def __repr__(self):
+        return f'<recipe id {self.recipe_id} has notes at note_id {self.note_id}: {self.notes}>'
 
 class Tag_Recipe_Relation(db.Model):
     """Relationship table for recipe tags and recipes"""
@@ -62,6 +70,10 @@ class Tag_Recipe_Relation(db.Model):
     r_tag_id = db.Column(db.Integer,
                             db.ForeignKey('recipe_tags.r_tag_id'), )
 
+    def __repr__(self):
+        return f'<tag-recipe-relationship id {self.tr_relation_id} is related to 
+        recipe_id {self.recipe_id} and recipe-tag id {self.r_tag_id}>'
+
 class Recipe_Tags(db.Model):
     """Tags to further identify recipes for the user"""
 
@@ -72,6 +84,9 @@ class Recipe_Tags(db.Model):
                         autoincrement = True, )
     r_tag_name = db.Column(db.String,
                         unique = True, ) #not sure if tag should be unique, but probably
+
+    def __repr__(self):
+        return f'<recipe-tag id {self.r_tag_id} is {self.r_tag_name}>'
 
 
 class Recipe_Ingredient_Relation(db.Model):
@@ -86,6 +101,10 @@ class Recipe_Ingredient_Relation(db.Model):
                             db.ForeignKey('recipes.recipe_id'), )
     ingredient_id = db.Column(db.Integer,
                             db.ForeignKey('ingredients.ingredient_id'), )
+    
+    def __repr__(self):
+        return f'<ingredient-recipe-relationship id {self.ri_relation_id} is related to 
+        recipe_id {self.recipe_id} and ingredient id {self.ingredient_id}>'
 
 
 class Ingredients(db.Model):
@@ -98,6 +117,8 @@ class Ingredients(db.Model):
                             autoicrement = True, )
     ingredient_name = db.Column(db.String,
                                 unique = True, )  #again, not sure if this should be unique
+    def __repr__(self):
+        return f'<ingredient_id {self.ingredient_id} is {ingredient_name}>'
 
 
 class Tag_Ingredient_Relation(db.Model):
@@ -112,6 +133,10 @@ class Tag_Ingredient_Relation(db.Model):
                                 db.ForeignKey('ingredients.ingredient_id'), )
     i_tag_id = db.Column(db.Integer,
                                 db.ForeignKey('ingredient_tags.i_tag_id'), )
+    
+    def __repr__(self):
+        return f'<tag-ingredient-relationship id {self.ti_relation_id} is
+         relates to ingredient_id {self.ingredient_id} and ingredient-tag id {self.i_tag_id}>'
 
 
 class Ingredient_Tags(db.Model):
@@ -121,6 +146,28 @@ class Ingredient_Tags(db.Model):
                         primary_key = True,
                         autoincrement = True, )
     i_tag_name = db.Column(db.String, )
-    
+
+    def __repr__(self):
+        return f'<ingredient tag id {i_tag_id} is {i_tag_name}>'
     
 
+
+def connect_to_db(flask_app, db_uri='postgresql:///recipes', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    #connect_to_db(app, echo=False)
