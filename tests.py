3d01e631dac3276b0584202c84c5df21b4bf89d1
vagrant <vagrant@vagrant.vm>
2021-04-20 20:15:01 +0000

    create tests.py to test my project

---
+++ tests.py
@@ -0,0 +1,4 @@
+"""Tests for crud.py and server.py"""
+
+from crud.py import (create_recipe, all_recipes, alphabetical_recipes, get_recipe_by_name, get_recipe_by_id, get_recipes_by_ingredient, get_recipes_by_tag, create_ingredient, add_ingredient_to_recipe, all_ingredients, alphabetical_ingredients, get_ingredient_by_id, get_ingredient_by_name, create_rating, create_note, create_recipe_tag, all_tags, alphabetical_tags, get_recipe_tag_by_id, get_recipe_tag_by_name, add_tag_to_recipe, get_recipes_by_multiple_ing, string_to_list, add_or_create_tag, add_or_create_ing )
+# from server.py import ()
